6f5c1bd8ba50b72d3d65390151acbc9e9fcaea4c
Martin CORNU <martin.cornu43@gmail.com>
2019-06-12 00:51:51 +0200

    first commit (scan elec panel outputs and detect which inputs are plugs)

---
+++ elec_panel_game.ino
@@ -0,0 +1,64 @@
+static int nb_pairs = 5;
+
+static int first_input = 8;   //Num. of first arduino input.
+static int first_output = 2;   //Num. of first arduino output.
+
+int8_t index = -1;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  
+  //Outputs 2-6
+  pinMode(first_output,OUTPUT);
+  pinMode(first_output+1,OUTPUT);
+  pinMode(first_output+2,OUTPUT);
+  pinMode(first_output+3,OUTPUT);
+  pinMode(first_output+4,OUTPUT);
+
+  //Inputs 8-12
+  pinMode(first_input,INPUT_PULLUP);
+  pinMode(first_input+1,INPUT_PULLUP);
+  pinMode(first_input+2,INPUT_PULLUP);
+  pinMode(first_input+3,INPUT_PULLUP);
+  pinMode(first_input+4,INPUT_PULLUP);
+}
+
+void loop() {
+
+  for (int i = 0; i<nb_pairs; i++) {
+    //Set i_output to low - like connected to ground for pullup inputs.
+    digitalWrite(first_output+i, LOW);
+    //Scan inputs for i_output
+    index = scan_inputs();
+    Serial.print(first_output+i);
+    Serial.print("eme elec tab input plug to : ");
+    Serial.print(index);
+    digitalWrite(first_output+i, HIGH);
+  }
+
+  Serial.println();
+  delay(1000);
+  
+}
+
+int8_t scan_inputs() {
+  int i = 0;
+  int res = -1;
+
+  bool isPlug = false;
+
+  while (i < nb_pairs && !isPlug) {
+    if (digitalRead(i+first_input) == 0) {
+      isPlug = true;
+      res = i+first_input;
+    }
+    i++;
+  }
+  
+  Serial.println("");
+
+  return res;
+}
+
